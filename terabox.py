87b4f5726fe813473bbb11607a8ba9985e12d6aa
golivesid <155578467+golivesid@users.noreply.github.com>
2024-07-12 23:16:23 +0530

    Update terabox.py

--- terabox.py
+++ terabox.py
@@ -8,8 +8,8 @@ from datetime import datetime
 # Initialize aria2p API client
 aria2 = aria2p.API(
     aria2p.Client(
-        host="http://localhost",
-        port=6800,
+        host="https://shaky-patrice-seeutech-49b950b3.koyeb.app/",
+        port=8000,
         secret=""
     )
 )
